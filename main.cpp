e041daf0fef819a0977995d9517a6bf565e70988
DevitChandraRoy <38016452+DevitChandraRoy@users.noreply.github.com>
2018-05-10 16:25:37 +0600

    update again

--- main.cpp
+++ main.cpp
@@ -10,8 +10,8 @@ int main()
       int a, b;
       a = -20;
       b = 10;
-      cout << div(a,b) << endl;
-      cout << mul_function(a,b) << endl;
-      cout << mod1(a) << " " << mod1(b) << endl;
+      cout << a << " / " << b << " = " << div(a,b) << endl;
+      cout << a << " * " << b << " = " <<mul_function(a,b) << endl;
+      cout << "|" <<a << "| = " << mod1(a) << ", " << "|" <<a << "| = " << mod1(b) << endl;
       return 0;
 }
