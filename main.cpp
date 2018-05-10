9fac032c476cc030379089015a1b688122f3c57e
DevitChandraRoy <38016452+DevitChandraRoy@users.noreply.github.com>
2018-05-10 16:15:07 +0600

    Update

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include<iostream>
 #include "div.h"
 #include "mul.h"
+#include "mod.h"
 
 using namespace std;
 
 int main()
 {
       int a, b;
-      a = 20;
+      a = -20;
       b = 10;
       cout << div(a,b) << endl;
       cout << mul_function(a,b) << endl;
-      
+      cout << mod1(a) << " " << mod1(b) << endl;
       return 0;
 }
